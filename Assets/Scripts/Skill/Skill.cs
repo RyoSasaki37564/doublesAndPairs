7e3c453ec3977e213b562d1189b1bca4b6c6c0ce
DESKTOP-4NV9MP5\vantan <sskryo0607@gmail.com>
2021-08-31 16:03:45 +0900

    UI調整

--- Assets/Scripts/Skill/Skill.cs
+++ Assets/Scripts/Skill/Skill.cs
@@ -4,5 +4,33 @@ using UnityEngine;
 
 public abstract class Skill : MonoBehaviour
 {
-    public abstract void Fazer();
+    [SerializeField] GameObject[] m_fazerSkills = new GameObject[3]; //フェイザーを格納
+
+    public enum Faze
+    {
+        firstFazer, //初期フェイザー
+        secondFazer, //一段階解放
+        lethalFazer, //最終段階解放
+    }
+
+    public static Faze m_faze;
+
+    // Start is called before the first frame update
+    public virtual void Fazer()
+    {
+        switch (m_faze)
+        {
+            case Faze.firstFazer:
+                Instantiate(m_fazerSkills[0], transform.position, transform.rotation);
+                break;
+            case Faze.secondFazer:
+                Instantiate(m_fazerSkills[1], transform.position, transform.rotation);
+                break;
+            case Faze.lethalFazer:
+                Instantiate(m_fazerSkills[2], transform.position, transform.rotation);
+                break;
+            default:
+                break;
+        }
+    }
 }
