48696c6f41ded79175c6cec7b3aa234f04330298
DESKTOP-4NV9MP5\vantan <sskryo0607@gmail.com>
2021-09-21 13:53:10 +0900

    キャラ選択画面ひな型製作中

---
+++ Assets/Home/Charas/TestCharaBox.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestCharaBox : MonoBehaviour
+{
+    [SerializeField] GameObject m_CHARAICON = default;
+
+    [SerializeField] LayoutGroup m_GL = default;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(var i = 0; i < 50; i++)
+        {
+            var x = Instantiate(m_CHARAICON);
+            x.transform.SetParent(m_GL.transform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
