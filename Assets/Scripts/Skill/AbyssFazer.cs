22ea79904295fecc5b92b8cc00f9fd847143cdc0
DESKTOP-4NV9MP5\vantan <sskryo0607@gmail.com>
2021-09-23 19:08:56 +0900

    フェイザー/アビスの魚影作成中

--- Assets/Scripts/Skill/AbyssFazer.cs
+++ Assets/Scripts/Skill/AbyssFazer.cs
@@ -1,14 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.EventSystems;
 
-public class TestFazer : Skill
+public class AbyssFazer : Skill
 {
-    string[] m_charaName = new string[]{ "ワルキューレの奇行" };
-    string[] m_fazeSetu = new string[] { "敵にやや強めの攻撃。", "敵から体力を吸収。", "「必殺ワルキューレミサイル」 " +
-        "パーティー最大HPの50%のダメージ。" };
+    string[] m_charaName = new string[] { "アビスの魚影" };
+    string[] m_fazeSetu = new string[] { "次に受けるダメージを半減。", "リーサルフェイザーを使うまでの間、水属性の威力が2倍になる。",
+        "「ディープブループレデター」 " +
+        "発動したターン中、1秒ごとに水属性エナジーを1個生成する。" };
 
     public void Update()
     {
