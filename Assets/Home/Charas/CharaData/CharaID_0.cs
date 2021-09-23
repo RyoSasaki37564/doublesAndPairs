5745d671e4cb5a305adf6f011b5fe38ed1ee18ed
DESKTOP-4NV9MP5\vantan <sskryo0607@gmail.com>
2021-09-24 06:13:22 +0900

    ガチャリザルト画面からキャラをパーティに入れてしまえるバグを解消

--- Assets/Home/Charas/CharaData/CharaID_0.cs
+++ Assets/Home/Charas/CharaData/CharaID_0.cs
@@ -12,67 +12,69 @@ public class CharaID_0 : UserCharaIcon
 
     public override void PartyIO()
     {
-        if(m_partyIO == false)
+        if (this.transform.parent.name == "CharaBox" || this.transform.parent.name == "PartyPanel")
         {
-            //メンバー選択したらステを足す
-            PartyParamator.m_hp = PartyParamator.m_hp + m_status[0];
-            PlayerPrefs.SetInt("PlayerHp", PartyParamator.m_hp);
+            if (m_partyIO == false)
+            {
+                //メンバー選択したらステを足す
+                PartyParamator.m_hp = PartyParamator.m_hp + m_status[0];
+                PlayerPrefs.SetInt("PlayerHp", PartyParamator.m_hp);
 
-            PartyParamator.m_fireButuri = PartyParamator.m_fireButuri + m_status[1];
-            PlayerPrefs.SetInt("RedS", PartyParamator.m_fireButuri);
+                PartyParamator.m_fireButuri = PartyParamator.m_fireButuri + m_status[1];
+                PlayerPrefs.SetInt("RedS", PartyParamator.m_fireButuri);
 
-            PartyParamator.m_fireMahou = PartyParamator.m_fireMahou + m_status[2];
-            PlayerPrefs.SetInt("RedC", PartyParamator.m_fireMahou);
+                PartyParamator.m_fireMahou = PartyParamator.m_fireMahou + m_status[2];
+                PlayerPrefs.SetInt("RedC", PartyParamator.m_fireMahou);
 
-            PartyParamator.m_iceButuri = PartyParamator.m_iceButuri + m_status[3];
-            PlayerPrefs.SetInt("BlueS", PartyParamator.m_iceButuri);
+                PartyParamator.m_iceButuri = PartyParamator.m_iceButuri + m_status[3];
+                PlayerPrefs.SetInt("BlueS", PartyParamator.m_iceButuri);
 
-            PartyParamator.m_iceMahou = PartyParamator.m_iceMahou + m_status[4];
-            PlayerPrefs.SetInt("BlueC", PartyParamator.m_iceMahou);
+                PartyParamator.m_iceMahou = PartyParamator.m_iceMahou + m_status[4];
+                PlayerPrefs.SetInt("BlueC", PartyParamator.m_iceMahou);
 
-            PartyParamator.m_woodButuri = PartyParamator.m_woodButuri + m_status[5];
-            PlayerPrefs.SetInt("GreenS", PartyParamator.m_woodButuri);
+                PartyParamator.m_woodButuri = PartyParamator.m_woodButuri + m_status[5];
+                PlayerPrefs.SetInt("GreenS", PartyParamator.m_woodButuri);
 
-            PartyParamator.m_woodMahou = PartyParamator.m_woodMahou + m_status[6];
-            PlayerPrefs.SetInt("GreenC", PartyParamator.m_woodMahou);
+                PartyParamator.m_woodMahou = PartyParamator.m_woodMahou + m_status[6];
+                PlayerPrefs.SetInt("GreenC", PartyParamator.m_woodMahou);
 
-            PlayerPrefs.Save();
+                PlayerPrefs.Save();
 
-            m_usingCharaID.Add(m_status[7]);
+                m_usingCharaID.Add(m_status[7]);
 
-            m_partyIO = true;
-            base.PartyIO();
+                m_partyIO = true;
+                base.PartyIO();
+            }
+            else
+            {
+                PartyParamator.m_hp = PartyParamator.m_hp - m_status[0];
+                PlayerPrefs.SetInt("PlayerHp", PartyParamator.m_hp);
+
+                PartyParamator.m_fireButuri = PartyParamator.m_fireButuri - m_status[1];
+                PlayerPrefs.SetInt("RedS", PartyParamator.m_fireButuri);
+
+                PartyParamator.m_fireMahou = PartyParamator.m_fireMahou - m_status[2];
+                PlayerPrefs.SetInt("RedC", PartyParamator.m_fireMahou);
+
+                PartyParamator.m_iceButuri = PartyParamator.m_iceButuri - m_status[3];
+                PlayerPrefs.SetInt("BlueS", PartyParamator.m_iceButuri);
+
+                PartyParamator.m_iceMahou = PartyParamator.m_iceMahou - m_status[4];
+                PlayerPrefs.SetInt("BlueC", PartyParamator.m_iceMahou);
+
+                PartyParamator.m_woodButuri = PartyParamator.m_woodButuri - m_status[5];
+                PlayerPrefs.SetInt("GreenS", PartyParamator.m_woodButuri);
+
+                PartyParamator.m_woodMahou = PartyParamator.m_woodMahou - m_status[6];
+                PlayerPrefs.SetInt("GreenC", PartyParamator.m_woodMahou);
+
+                PlayerPrefs.Save();
+
+                m_usingCharaID.Remove(m_status[7]);
+
+                m_partyIO = false;
+                base.PartyIO();
+            }
         }
-        else
-        {
-            PartyParamator.m_hp = PartyParamator.m_hp - m_status[0];
-            PlayerPrefs.SetInt("PlayerHp", PartyParamator.m_hp);
-
-            PartyParamator.m_fireButuri = PartyParamator.m_fireButuri - m_status[1];
-            PlayerPrefs.SetInt("RedS", PartyParamator.m_fireButuri);
-
-            PartyParamator.m_fireMahou = PartyParamator.m_fireMahou - m_status[2];
-            PlayerPrefs.SetInt("RedC", PartyParamator.m_fireMahou);
-
-            PartyParamator.m_iceButuri = PartyParamator.m_iceButuri - m_status[3];
-            PlayerPrefs.SetInt("BlueS", PartyParamator.m_iceButuri);
-
-            PartyParamator.m_iceMahou = PartyParamator.m_iceMahou - m_status[4];
-            PlayerPrefs.SetInt("BlueC", PartyParamator.m_iceMahou);
-
-            PartyParamator.m_woodButuri = PartyParamator.m_woodButuri - m_status[5];
-            PlayerPrefs.SetInt("GreenS", PartyParamator.m_woodButuri);
-
-            PartyParamator.m_woodMahou = PartyParamator.m_woodMahou - m_status[6];
-            PlayerPrefs.SetInt("GreenC", PartyParamator.m_woodMahou);
-
-            PlayerPrefs.Save();
-
-            m_usingCharaID.Remove(m_status[7]);
-
-            m_partyIO = false;
-            base.PartyIO();
-        }
-
     }
 }
