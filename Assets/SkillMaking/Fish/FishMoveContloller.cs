612e1103ad97148a47efc5996ab005e69432912f
DESKTOP-4NV9MP5\vantan <sskryo0607@gmail.com>
2021-09-23 17:46:05 +0900

    魚のアニメーション完成

--- Assets/SkillMaking/Fish/FishMoveContloller.cs
+++ Assets/SkillMaking/Fish/FishMoveContloller.cs
@@ -6,7 +6,7 @@ public class FishMoveContloller : MonoBehaviour
 {
     Rigidbody2D m_fishBody;
     SpriteRenderer m_fishSpr;
-    float m_speed = -1f;
+    [SerializeField]float m_speed = -0.001f;
     float rondom;
 
     // Start is called before the first frame update
@@ -19,22 +19,19 @@ public class FishMoveContloller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        rondom = Random.Range(-0.1f, 0.1f);
         m_fishBody.AddForce(new Vector2(m_speed, rondom), ForceMode2D.Force);
-        rondom = Random.Range(-2, 3);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "OutLine")
         {
+            rondom = Random.Range(-0.3f, 0.3f);
             m_speed *= -1;
             this.gameObject.transform.localScale = new Vector3(this.gameObject.transform.localScale.x * -1, this.gameObject.transform.localScale.y,
                 this.gameObject.transform.localScale.z);
         }
 
-        if (collision.gameObject.tag == "Respawn")
-        {
-            rondom = Random.Range(-2, 3);
-        }
     }
 }
