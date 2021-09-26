366977975e74f57e25c7e16a42fc710ca33ea490
DESKTOP-4NV9MP5\vantan <sskryo0607@gmail.com>
2021-09-27 06:12:33 +0900

    BGM演出調整開始

--- Assets/Scripts/Skill/Nyux/NyuxFazer.cs
+++ Assets/Scripts/Skill/Nyux/NyuxFazer.cs
@@ -6,7 +6,7 @@ public class NyuxFazer : Skill
 {
     string[] m_charaName = new string[] { "闇の魔女・ニュクス", "宵闇の女神・ニュクス" };
     string[] m_fazeSetu = new string[] { "「エナジーマトリックス・術」 " + "プレイヤーターン中、5秒ごとに魔法エナジーを1個生成する。",
-        "体力を全回復。", "「闇夜」 " +
+        "最大体力の30%を回復。", "「闇夜」 " +
         "1ターンの間魔法攻撃力1.5倍、操作時間を7秒増やす。(エナジー操作前のみ発動可能)(時間増加は重複不可)" };
 
     public static bool m_NyuxCanUse = false;
