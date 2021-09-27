8908bfbc1e7207f45bd911b474db611266cc2521
DESKTOP-4NV9MP5\vantan <sskryo0607@gmail.com>
2021-09-27 17:34:43 +0900

    図鑑データとりあえず完成

---
+++ Assets/Home/Charas/ZukanData/Zukan_4.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Zukan_4 : ZukansParamator
+{
+    [SerializeField] Text m_skillSetumei = default; //パネルの説明入れ
+
+    [SerializeField] NyuxFazer m_thisFazer = default; //こやつのスキル
+
+    string m_faze1, m_faze2, m_lethal;
+
+    [SerializeField] GameObject m_charaStats = default; //キャラステ表示パネル
+
+    public override void DataPanneling()
+    {
+        m_faze1 = m_thisFazer.m_fazeSetu[0];
+        m_faze2 = m_thisFazer.m_fazeSetu[1];
+        m_lethal = m_thisFazer.m_fazeSetu[2];
+
+        m_skillSetumei.text = m_faze1 + "\n" + m_faze2 + "\n" + m_lethal;
+
+        base.DataPanneling();
+
+        m_charaStats.SetActive(true);
+    }
+}
