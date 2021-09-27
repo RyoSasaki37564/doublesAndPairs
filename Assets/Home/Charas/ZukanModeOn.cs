2958d93d54a9009c6e71582007524dadda0f03cb
DESKTOP-4NV9MP5\vantan <sskryo0607@gmail.com>
2021-09-27 14:01:07 +0900

    図鑑モード制作中

---
+++ Assets/Home/Charas/ZukanModeOn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZukanModeOn : MonoBehaviour
+{
+    [SerializeField] GameObject m_Zukan = default; //図鑑
+    bool m_flg = false;
+    
+    public void ZukanMode()
+    {
+        if(m_flg == false)
+        {
+            m_Zukan.SetActive(true);
+            m_flg = true;
+        }
+        else
+        {
+            m_Zukan.SetActive(false);
+            m_flg = false;
+        }
+        
+    }
+
+}
---
+++ Assets/Home/Charas/ZukansParamator.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ZukansParamator : MonoBehaviour
+{
+    [SerializeField] CharaID m_thisCharasStatus = default; //そのキャラのパラメーター
+
+    //各パラメータのテキスト。 name, hp, fa. fm, ia, im. wa, wm, skill.
+    [SerializeField] Text[] m_paramatorText = new Text[9];
+
+    [SerializeField] GameObject m_charaStats = default; //キャラステ表示パネル
+
+    string m_statsType; //すてーたすしゅべつ
+
+    [SerializeField] string m_onamae = default;
+
+
+
+   public void DataPanneling()
+    {
+        m_paramatorText[0].text = m_onamae;
+
+        // ステータス表示
+        for (var i = 1; i < 8; i++)
+        {
+            if (i == 1)
+            {
+                m_statsType = "体力";
+            }
+            else if (i == 2)
+            {
+                m_statsType = "火属性攻撃力";
+            }
+            else if (i == 3)
+            {
+                m_statsType = "火属性魔法力";
+            }
+            else if (i == 4)
+            {
+                m_statsType = "水属性攻撃力";
+            }
+            else if (i == 5)
+            {
+                m_statsType = "水属性魔法力";
+            }
+            else if (i == 6)
+            {
+                m_statsType = "木属性攻撃力";
+            }
+            else
+            {
+                m_statsType = "木属性魔法力";
+            }
+
+            m_paramatorText[i].text =  m_statsType + " : " +  m_thisCharasStatus.m_status[i - 1].ToString();
+        }
+        m_charaStats.SetActive(true);
+    }
+}
