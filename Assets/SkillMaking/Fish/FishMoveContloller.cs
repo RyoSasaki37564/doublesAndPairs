2734822d177cf8861612633bcc5fa893b3b5500a
DESKTOP-4NV9MP5\vantan <sskryo0607@gmail.com>
2021-09-28 22:52:37 +0900

    微調整

--- Assets/SkillMaking/Fish/FishMoveContloller.cs
+++ Assets/SkillMaking/Fish/FishMoveContloller.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class FishMoveContloller : MonoBehaviour
 {
     Rigidbody2D m_fishBody;
-    //SpriteRenderer m_fishSpr;
     [SerializeField]float m_speed = -0.001f;
     float rondom;
 
@@ -13,7 +12,6 @@ public class FishMoveContloller : MonoBehaviour
     void Start()
     {
         m_fishBody = this.gameObject.GetComponent<Rigidbody2D>();
-        //m_fishSpr = this.gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -29,7 +27,8 @@ public class FishMoveContloller : MonoBehaviour
         {
             rondom = Random.Range(-0.3f, 0.3f);
             m_speed *= -1;
-            this.gameObject.transform.localScale = new Vector3(this.gameObject.transform.localScale.x * -1, this.gameObject.transform.localScale.y,
+            this.gameObject.transform.localScale = new Vector3(this.gameObject.transform.localScale.x * -1, 
+                this.gameObject.transform.localScale.y,
                 this.gameObject.transform.localScale.z);
         }
 
