922d8b9db25ab9b7656f0537cb678ed99e67f30f
DESKTOP-4NV9MP5\vantan <sskryo0607@gmail.com>
2021-09-29 16:22:23 +0900

    キャラが一体いなくなるバグを直したいですな。

--- Assets/Scripts/CharaIconManager.cs
+++ Assets/Scripts/CharaIconManager.cs
@@ -8,16 +8,16 @@ public class CharaIconManager : MonoBehaviour
     [SerializeField] GameObject[] m_usingChara = new GameObject[CharaID.m_usingCharaID.Count];
     [SerializeField] GameObject[] party = new GameObject[CharaID.m_usingCharaID.Count];
     [SerializeField] LayoutGroup m_deck = default;
+    int id; //ID番号を数値化するための容器
 
 
     // Start is called before the first frame update
     void Start()
     {
-        for(var i = 0; i < party.Length; i++)
+        for (var i = 0; i < party.Length; i++)
         {
-            int id = CharaID.m_usingCharaID[i];
-            var x = Instantiate(m_usingChara[id]);
-            party[i] = x;
+            id = CharaID.m_usingCharaID[i];
+            party[i] = Instantiate(m_usingChara[id]);
             party[i].SetActive(true);
             party[i].transform.SetParent(m_deck.transform);
             party[i].transform.localScale = new Vector3(1, 1, 0);
