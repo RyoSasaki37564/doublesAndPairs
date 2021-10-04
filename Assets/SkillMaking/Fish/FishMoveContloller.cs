32a24948f786bdd6e97ea15a5e8872be69bcb1ae
DESKTOP-4NV9MP5\vantan <sskryo0607@gmail.com>
2021-10-04 12:04:02 +0900

    Fixedレートをいじりながらビルド時のベストレートを模索。

--- Assets/SkillMaking/Fish/FishMoveContloller.cs
+++ Assets/SkillMaking/Fish/FishMoveContloller.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class FishMoveContloller : MonoBehaviour
 {
     Rigidbody2D m_fishBody;
-    [SerializeField]float m_speed = -0.001f;
+    [SerializeField] float m_speed = -0.001f;
+    [SerializeField] float m_ranges = 0.03f;
     float rondom;
 
     // Start is called before the first frame update
@@ -17,7 +18,7 @@ public class FishMoveContloller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rondom = Random.Range(-0.1f, 0.1f);
+        rondom = Random.Range(-1*m_ranges/3, m_ranges/3);
         m_fishBody.AddForce(new Vector2(m_speed, rondom), ForceMode2D.Force);
     }
 
@@ -25,7 +26,7 @@ public class FishMoveContloller : MonoBehaviour
     {
         if (collision.gameObject.tag == "OutLine")
         {
-            rondom = Random.Range(-0.3f, 0.3f);
+            rondom = Random.Range(-1*m_ranges, m_ranges);
             m_speed *= -1;
             this.gameObject.transform.localScale = new Vector3(this.gameObject.transform.localScale.x * -1, 
                 this.gameObject.transform.localScale.y,
