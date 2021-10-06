fe1ad4000ee358fd6e43593b8419c04c997601d5
DESKTOP-4NV9MP5\vantan <sskryo0607@gmail.com>
2021-10-06 15:14:15 +0900

    データ削除機能開発中。

--- Assets/Home/Menus/MenuManager.cs
+++ Assets/Home/Menus/MenuManager.cs
@@ -10,6 +10,7 @@ public class MenuManager : MonoBehaviour
         Quest,
         Chara,
         Gacha,
+        Option,
     }
     public static HomeMenu menu;
 
@@ -21,16 +22,18 @@ public class MenuManager : MonoBehaviour
     /// 各メニューの実体化、隠蔽をスムーズにするために、それぞれのメニューに親オブジェクトを設定。それにSetActiveをかける。
     /// 要素は0からクエスト、キャラ、ガチャ。
     /// </summary>
-    [SerializeField] GameObject[] m_menuObj = new GameObject[3];
+    [SerializeField] GameObject[] m_menuObj = new GameObject[4];
 
-    [SerializeField] Color[] m_color = new Color[3];
+    [SerializeField] Color[] m_color = new Color[4];
 
     // Start is called before the first frame update
     void Start()
     {
+        m_soul = 100;
         m_menuObj[0].SetActive(false);
         m_menuObj[1].SetActive(false);
         m_menuObj[2].SetActive(false);
+        m_menuObj[3].SetActive(false);
         menu = HomeMenu.Quest;
         m_MenuHeadText[0].text = menu.ToString();
         m_MenuHeadText[1].text = "所持ソウル: "+ m_soul.ToString();
@@ -45,6 +48,7 @@ public class MenuManager : MonoBehaviour
                 m_menuObj[0].SetActive(true);
                 m_menuObj[1].SetActive(false);
                 m_menuObj[2].SetActive(false);
+                m_menuObj[3].SetActive(false);
                 m_MenuHeadText[0].text = menu.ToString();
                 m_MenuHeadText[0].color = m_color[0];
                 m_MenuHeadText[1].text = "所持ソウル: " + m_soul.ToString();
@@ -54,19 +58,30 @@ public class MenuManager : MonoBehaviour
                 m_menuObj[0].SetActive(false);
                 m_menuObj[1].SetActive(true);
                 m_menuObj[2].SetActive(false);
+                m_menuObj[3].SetActive(false);
                 m_MenuHeadText[0].text = menu.ToString();
                 m_MenuHeadText[0].color = m_color[1];
-                m_MenuHeadText[1].text = "所持ソウル: " + m_soul.ToString();
                 break;
 
             case HomeMenu.Gacha:
                 m_menuObj[0].SetActive(false);
                 m_menuObj[1].SetActive(false);
                 m_menuObj[2].SetActive(true);
+                m_menuObj[3].SetActive(false);
                 m_MenuHeadText[0].text = menu.ToString();
                 m_MenuHeadText[0].color = m_color[2];
                 m_MenuHeadText[1].text = "所持ソウル: " + m_soul.ToString();
                 break;
+
+            case HomeMenu.Option:
+                m_menuObj[0].SetActive(false);
+                m_menuObj[1].SetActive(false);
+                m_menuObj[2].SetActive(false);
+                m_menuObj[3].SetActive(true);
+                m_MenuHeadText[0].text = menu.ToString();
+                m_MenuHeadText[0].color = m_color[3];
+                break;
+
         }
     }
 }
---
+++ Assets/Home/Option/MenuSetOption.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSetOption : MonoBehaviour
+{
+    public void MenuToOption()
+    {
+        MenuManager.menu = MenuManager.HomeMenu.Option;
+    }
+}
