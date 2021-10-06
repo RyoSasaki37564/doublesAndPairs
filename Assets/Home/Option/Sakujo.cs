e2971c2826f2ec5f55f98d0909a59194371a9821
DESKTOP-4NV9MP5\vantan <sskryo0607@gmail.com>
2021-10-06 15:35:59 +0900

    Optionのひな型完成

--- Assets/Home/Option/Sakujo.cs
+++ Assets/Home/Option/Sakujo.cs
@@ -7,6 +7,8 @@ public class Sakujo : MonoBehaviour
 {
    public void DataDestroy()
     {
+        UserCharaIcon.m_partyNum = 0;
+        CharaID.m_usingCharaID.Clear();
         SceneManager.LoadScene("Title");
     }
 }
